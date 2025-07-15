fc79085b7a053d0b193081e1a7958018c79d0d39
karthik-k13 <karthik.krishnareddy.mahadi@gmail.com>
2025-07-15 08:58:54 +0530

    Update MkrfintechApplication.java

--- src/main/java/com/mkr/mkrfintech/MkrfintechApplication.java
+++ src/main/java/com/mkr/mkrfintech/MkrfintechApplication.java
@@ -8,7 +8,7 @@ public class MkrfintechApplication {
 
 	public static void main(String[] args) {
 		String branch="main";
-		String subBranch="main-1234";
+		String subBranch="release";
 		SpringApplication.run(MkrfintechApplication.class, args);
 
 	}
