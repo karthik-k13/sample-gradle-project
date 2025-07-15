2e4a78e699e873ee1ad2ccb6aa0dcd2041153638
karthik-k13 <karthikreddy55015@gmail.com>
2025-07-15 08:39:24 +0530

    initial commit

---
+++ src/main/java/com/mkr/mkrfintech/MkrfintechApplication.java
@@ -0,0 +1,15 @@
+package com.mkr.mkrfintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkrfintechApplication {
+
+	public static void main(String[] args) {
+		String branch="main";
+		SpringApplication.run(MkrfintechApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/mkr/mkrfintech/MkrfintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkr.mkrfintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkrfintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
